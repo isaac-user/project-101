17c8e9c40992193e7e751a34e8f785d0a78d23c0
isaac-user <114296300+isaac-user@users.noreply.github.com>
2023-12-13 16:40:46 +0000

    Add files via upload

---
+++ project 101.py
@@ -0,0 +1,44 @@
+import random
+rollingdie = str (input("Do you want to roll the dice"))
+while response == "y": 
+      no = random.radiant(1,6)
+      radiant(start,end)     
+      
+
+no = random.randint(1,6) # conditions to check the number value
+if no == 1:
+    print("[-----]")
+    print("[ ]") 
+    print("[ 0 ]") 
+    print("[ ]") 
+    print("[-----]")
+if no == 2: 
+    print("[-----]")
+    print("[ 0 ]")
+    print("[ ]")
+    print("[ 0 ]") 
+    print("[-----]")
+if no == 3:
+    print("[-----]")
+    print("[ ]")
+    print("[0 0 0]") 
+    print("[ ]")
+    print( "[-----]")
+if no == 4:
+        print("[-----]")
+        print("[0 0]") 
+        print("[ ]") 
+        print("[0 0]")
+        print("[-----]")
+if no == 5:
+        print("[-----]")
+        print("[0 0]")
+        print("[ 0 ]")
+        print("[0 0]")
+        print("[-----]")
+if no == 6:
+     print("[-----]")
+     print("[0 0 0]") 
+     print("[ ]")
+     print("[0 0 0]")
+     print("[-----]")
